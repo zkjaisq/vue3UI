45cacb801e7a6120647b1f30343094417d80c4e5
zhangkejia <18771964684@163.com>
2022-05-16 09:59:12 +0800

    feat(create dialog component): 创建dialog组件

---
+++ src/lib/Dialaog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="dialog-overlay">
+    <div class="dialog-wrapper">
+      <header>标题</header>
+      <main>
+        <p>文本一</p>
+        <p>文本二</p>
+      </main>
+      <footer>
+        <Button>ok</Button>
+        <Button>cancel</Button>
+      </footer>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import Button from "../lib/Button.vue"
+export default {
+  components: {
+    Button
+  }
+}
+</script>
